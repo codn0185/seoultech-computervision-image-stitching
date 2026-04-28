739daebe7096265bf065baf8836d237a03cf94f3
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-04-28 14:07:31 +0900

    feat: 앱 주요 메서드 추가

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from image_stitching import ImageStitching
 
+images: list = [
+    "images/img1.png",
+    "images/img2.png",
+    "images/img3.png",
+]
+
 
 def main():
     app = ImageStitching()
-    app.run()
+    app.run(images)
 
 
 if __name__ == "__main__":
--- image_stitching.py
+++ image_stitching.py
@@ -1,6 +1,61 @@
+import cv2
+import numpy as np
+
+
 class ImageStitching:
+    # === 샹수 ===
+
+    WINDOW_TITLE: str = "Image Stitcher"  # 윈도우 제목
+    RESULT_FILENAME: str = "Result.png"  # 저장할 파일명
+
+    # === 멤버 변수 ===
+
+    detector: cv2.Feature2D  # Feature Detector
+    matcher: cv2.DescriptorMatcher  # Feature Matcher
+
+    images: list = []  # 사용할 이미지 리스트
+    result_image: np.ndarray  # 결과 이미지
+
+    # 생성자
     def __init__(self):
+        # Initialize Feature Detector
+        self.detector = cv2.SIFT_create()  # SIFT
+        # Initialize Feature Matcher
+        self.matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE)
+
+    # 프로그램 실행
+    def run(self, image_files: list[str] = []):
+        self.images = self.load_image_files(image_files)
+        if not self.images:
+            raise Exception("Empty Image List")
+
+        # 이미지 정합
+        self.stitch_images()
+
+        # 결과 저장 및 출력
+        self.save_result()
+        self.show_result()
+
+    # 이미지 파일 불러오기
+    def load_image_files(self, filenames: list[str]):
+        imgs = []
+        for filename in filenames:
+            try:
+                imgs.append(cv2.imread(filename))
+            except:
+                continue
+        return imgs
+
+    # 이미지 정합
+    def stitch_images(self):
         pass
 
-    def run(self):
-        pass
+    # 결과 저장
+    def save_result(self):
+        cv2.imwrite(self.RESULT_FILENAME, self.result_image)
+
+    # 결과 출력
+    def show_result(self):
+        cv2.imshow(self.WINDOW_TITLE, self.result_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
